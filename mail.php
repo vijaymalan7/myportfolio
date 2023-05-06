5ec8b0724e6f47ee0afa21acda748dbfae0f4228
vijaymalan7 <131236444+vijaymalan7@users.noreply.github.com>
2023-05-06 21:22:44 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php
+$name= $_POST['name'];
+$email= $_POST['email'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+$to= "vijaymalan12@gmail.com" ;
+$subject ="mail from myportfolio";
+$txt ="name= ". $name . "\r\n Email =" . $email . "\r\n Subject=" .$subject.  "\r\n Message = " .$messge;
+$headers ="From: vijaymalan7.github.io/myportfolio/";
+if($email !=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+header("thankyou.html");
+?>
