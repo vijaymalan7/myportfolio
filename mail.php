f9e45e795cba7b091154ac1fd3d53a1c40dd1ecd
vijaymalan7 <131236444+vijaymalan7@users.noreply.github.com>
2023-05-07 07:21:33 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,9 +6,12 @@ $message= $_POST['message'];
 $to= "vijaymalan12@gmail.com" ;
 $subject ="mail from myportfolio";
 $txt ="name= ". $name . "\r\n Email =" . $email . "\r\n Subject=" .$subject.  "\r\n Message = " .$messge;
-$headers ="From: vijaymalan7.github.io/myportfolio/";
+$headers ="From: $name < $email> ";
 if($email !=NULL){
     mail($to,$subject,$txt,$headers);
+    echo "<script type='text/javascript'>alert('your message sent successfully');
+    window.history.log(-1);
+    </script>";
 }
-header("Location: thankyou.html");
+//header("Location: thankyou.html");
 ?>
